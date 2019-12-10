5395a6a2c08829641b6f216f226990357d04eb9e
Expproletariy <expproletariy@gmail.com>
2019-12-11 01:30:06 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon/>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import './plugins/axios'
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  // @ts-ignore
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
